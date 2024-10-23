46a6154d258ed841331a5b288b12ce36d8bbece1
Justin Liang <justliang100@gmail.com>
2024-10-23 09:32:49 -1000

    ReadData now returns dictionary

--- src/utilities/Read_json.py
+++ src/utilities/Read_json.py
@@ -1,6 +1,5 @@
 import json
-import Meters
-import infrastructure as inf
+
 
 # Open and read the JSON file
 # with open(r'Register_Dictionary_EPM7000.JSON', 'r') as file:
@@ -14,19 +13,19 @@ def Read_data( targetMeter:str, Data_Value):
     if targetMeter == 'PQMII':
         with open(r'Register_Dictionary_PQMII.JSON', 'r') as file:
             data = json.load(file)
-    elif targetMeter.metertype == 'EPM7000':
+    elif targetMeter == 'EPM7000':
         with open(r'Register_Dictionary_EPM7000.JSON', 'r') as file:
             data = json.load(file)
 
     Value_Holder = []
 
     x = data["Registers"][Data_Value][0]
-    for i in x:
+    for i in x.values():
         Value_Holder.append(i)
 
     #change this to return the list of data in the json entry: address, coils, units, etc.
-    print(Value_Holder)
-    register = "placeholder"
-    return register
+    print(x)
+    return x
+
 
 Read_data("EPM7000","3 phase watt total")
