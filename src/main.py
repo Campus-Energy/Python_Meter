92ebc07d60d7a65137a4f97a602d5623e069f3a1
Justin Liang <45135906+MakeFolderForFiles@users.noreply.github.com>
2025-02-24 16:15:54 -1000

    Created a test main and moved uncomplement to function corner

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 from PQMII.PQMII_module import PQM_Meter
 
+import utilities
+import utilities.Meters
+import utilities.infrastructure
+
+aloha = utilities.Meters( metername='aloha', metertype=utilities.infrastructure.meterType.PQMII ,host = 'host', measurements=['time','kw'], port = 4, addressBook={})
+
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
     main()
     
-import utilities
 
-import utilities.Meters
-import utilities.infrastructure
 
-aloha = utilities.Meters( metername='aloha', metertype=utilities.infrastructure.meterType.PQMII ,host = 'host', measurements=['time','kw'], port = 4, addressBook={})
-def main():
-    print(aloha.metername)
+
     
--- src/utilities/uncomplement.py
+++
@@ -1,8 +0,0 @@
-#takes in the raw string value from a register and uncomplements them. This returns a list with the following format: [firstByte,secondByte,combined]
-def uncomplement ( twosComplement :str ):
-                twosComplementBinary = format ( abs(~(int(twosComplement) - 1)), '016b' )
-                firstByte = ( int( twosComplementBinary, base = 2 ) & 0b1111111100000000 ) >> 8
-                secondByte = int( twosComplementBinary, base = 2 ) & 0b0000000011111111
-                uncomplementedNum = str( firstByte ) + str ( secondByte )
-                
-                return [firstByte,secondByte,uncomplementedNum]
