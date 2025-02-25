fbeea7364c4e1857accb02ad559113cd588b2c51
Justin Liang <45135906+MakeFolderForFiles@users.noreply.github.com>
2025-02-24 16:18:17 -1000

    Undid uncomplement relocation because it's being called in meters.py

--- src/test_main.py
+++ src/test_main.py
@@ -3,7 +3,7 @@ from utilities.Meters import Meter
 from utilities import infrastructure
 from utilities import Read_json
 
-
+# A test main script to test individual modules, maybe create seperate branch on github for this purpose
 
 aloha = Meter( metername='aloha', metertype=infrastructure.meterType.PQMII ,host = 'host', measurements=['time','kw'], port = 4, addressBook={})
 
