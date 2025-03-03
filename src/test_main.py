6291bdd37660f3f930e38bb97e541c7541e42050
Fubuma <122340149+BFubumaK@users.noreply.github.com>
2025-03-03 09:10:08 -1000

    Added MeterDeets

--- src/test_main.py
+++ src/test_main.py
@@ -6,11 +6,18 @@ from pathlib import Path
 
 # A test main script to test individual modules, maybe create seperate branch on github for this purpose
 # Using POST CHILLER
-aloha = Meter( metername='ADMIN_SERV_2', metertype=infrastructure.meterType.EPM7000 ,host = '10.181.5.140', measurements=['time','kw'], port=502,slave=1)
+adminServ2 = Meter( metername='ADMIN_SERV_2', metertype=infrastructure.meterType.EPM7000 ,host = '10.181.5.140', measurements=['time','kw'], port=502,slave=1)
+
 
 
 
 def main():
+    # loop throught a  file containing all the meter names, metertypes, ips, measurements, ports, and slave.
+    #meterList = read meter name list
+    #for meter in meterList
+    #   curretMeter = Meter( metername = meterList[metername], metertype = meterList[metertype], host = meterList[host], measurements = meterList[measurements], etc )
+    #   currentData = currentMeter.getData()
+    #   append currenData to csv 
     print("Hello World!")
 
 
